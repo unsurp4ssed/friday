c601810576b059dc3f5456888ea34f62f3ead38a
unsurp4ssed <116448397+unsurp4ssed@users.noreply.github.com>
2025-07-19 17:12:26 +0300

    Update manual-v4l2.sh

--- series-test/manual-v4l2.sh
+++ series-test/manual-v4l2.sh
@@ -6,7 +6,7 @@ echo "Started:" `date`
 
 for i in {1..20}
 do
-    v4l2-ctl -d /dev/video0 --set-fmt-video=width=1920,height=1080,pixelformat=MJPG --stream-mmap --stream-to=./$test_directory=test-series-manual$i.jpg --stream-count=1
+    v4l2-ctl -d /dev/video0 --set-fmt-video=width=1920,height=1080,pixelformat=MJPG --stream-mmap --stream-to=./$test_directory/test-series-manual$i.jpg --stream-count=1
     sleep 1s
     echo "test-series-manual$i.jpg done..."
 done
