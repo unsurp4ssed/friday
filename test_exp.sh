df274b12e334c692fa2ad5765f364ba39e400760
unsurp4ssed <116448397+unsurp4ssed@users.noreply.github.com>
2025-07-31 17:11:31 +0300

    Update test_exp.sh

--- test_exp.sh
+++ test_exp.sh
@@ -14,12 +14,12 @@ do
     if pgrep -x "ffmpeg" > /dev/null #check if ffmpeg stream to dummy camera is running
     then
         #save a frame in bmp
-        ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video2 -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $photo_filename -hide_banner -loglevel error
+        ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video2 -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $test_directory/test_exp$i.bmp -hide_banner -loglevel error
     else
         #copy stream from real camera to dummy
         ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video0 -codec copy -f video4linux2 /dev/video2 -hide_banner -loglevel error &
         #save a frame in bmp
-        ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video2 -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $photo_filename -hide_banner -loglevel error
+        ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video2 -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $test_directory/test_exp$i.bmp -hide_banner -loglevel error
     fi
     echo -e " done."
 done
