c2f6945c9349ffe50e7005e6975a887aeaaf8b62
unsurp4ssed <116448397+unsurp4ssed@users.noreply.github.com>
2025-07-31 15:32:48 +0300

    Update test_exp.sh

--- test_exp.sh
+++ test_exp.sh
@@ -10,8 +10,17 @@ for i in {100..1200..100}
 do
     echo -n "Exposure setting: $i..."
     v4l2-ctl --set-ctrl exposure_time_absolute=$i
-    #save a frame in bmp
-    ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video2 -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $test_directory/test_exp$i.bmp -hide_banner -loglevel error
+
+    if pgrep -x "ffmpeg" > /dev/null #check if ffmpeg stream to dummy camera is running
+    then
+        #save a frame in bmp
+        ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video2 -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $photo_filename -hide_banner -loglevel error
+    else
+        #copy stream from real camera to dummy
+        ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video0 -codec copy -f video4linux2 /dev/video2 -hide_banner -loglevel error &
+        #save a frame in bmp
+        ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video2 -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $photo_filename -hide_banner -loglevel error
+    fi
     echo -e " done."
 done
 
