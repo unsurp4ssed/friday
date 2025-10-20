7d9b31c5287ff0e08fc4279dbc91845a8fd83c7d
unsurp4ssed <116448397+unsurp4ssed@users.noreply.github.com>
2025-10-20 15:49:24 +0300

    Update test_exp-n-gain.sh

--- test_exp-n-gain.sh
+++ test_exp-n-gain.sh
@@ -29,11 +29,12 @@ echo "Started:" `date`
 for exp in {100..1200..100}
 do
     echo -n "Exposure setting: $exp..."
-    v4l2-ctl --set-ctrl --device $device exposure_time_absolute=$exp
+    v4l2-ctl --device $device --set-ctrl  exposure_time_absolute=$exp
 
     for gain in {0..9..1} {10..50..5}
     do
     echo -n "Gain setting: $gain..."
+      v4l2-ctl --device $device --set-ctrl gain=$gain
       #save a frame in bmp
       ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i $device -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $test_directory/test_exp$exp.gain$gain.bmp -hide_banner -loglevel error
       echo -e " done."
