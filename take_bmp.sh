61a3fdf8746f8291643df8bfb6abac88daf14870
unsurp4ssed <116448397+unsurp4ssed@users.noreply.github.com>
2025-10-20 15:42:52 +0300

    Update take_bmp.sh

--- take_bmp.sh
+++ take_bmp.sh
@@ -6,28 +6,33 @@ if [ ! $# -eq 0 ]
     photo_filename="$1"
 fi
 
+device="/dev/video"
+
+for i in {0..10}
+do
+        if sudo grep -q 2688 /sys/class/video4linux/video$i/name 2>/dev/null;  then
+                device+=$i
+                break
+        fi
+        if [ $i -eq 10 ]; then
+                echo -n "searched /dev/video 0 through 10, havent found star sensor camera (SPCA2688 AV Camera: SPCA2688 AV). Exiting.."
+                exit 1
+        fi
+done
+
+
 echo "Started:" `date`
 echo -n "Changing exposure control to manual mode..."
-v4l2-ctl --set-ctrl auto_exposure=1 #1 - manual exposure mode 
+v4l2-ctl --device $device --set-ctrl auto_exposure=1 #1 - manual exposure mode 
 echo "done"
 
 #setting exposure from config file
 echo -n "Exposure setting: $exposure..."
-v4l2-ctl --set-ctrl exposure_time_absolute=$exposure
+v4l2-ctl --device $device --set-ctrl exposure_time_absolute=$exposure
 echo -e " done."
 
-if ! v4l2-ctl --list-devices | grep "Dummy" > /dev/null #check if dummy camera was already created
-then
-sudo modprobe v4l2loopback #create dummy camera device
-fi
 
-if ! pgrep -x "ffmpeg" > /dev/null #check if ffmpeg stream to dummy camera is running
-then
-    #copy stream from real camera to dummy
-    ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video0 -codec copy -f video4linux2 /dev/video2 -hide_banner -loglevel error &
-fi
-#save a frame in bmp
-ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i /dev/video2 -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $photo_filename -hide_banner -loglevel error
+ffmpeg -f video4linux2 -video_size 1920x1080 -input_format yuyv422 -i $device -c:v bmp -f image2 -pix_fmt bgr24 -frames:v 1 pipe:1 > $photo_filename -hide_banner -loglevel error 
 
 
 echo -e "\nFinished:" `date`
